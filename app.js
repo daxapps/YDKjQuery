347f6aa0b0e46ae0c3070f8eead95d229f21eb54
Jason Crawford <daxapps777@gmail.com>
2017-05-18 16:37:46 -0500

    Refactor

--- app.js
+++ app.js
@@ -1,129 +1,146 @@
 
 var state = {
-  items: []
+  questions: [
+  	{
+		question: "Create a new jQuery object with elements added to the set of matched elements.",
+		choices: [".add()", ".eq()", ".find()", ".delay()"],
+		correctAnswer: 0
+	}, {
+		question: "Reduce the set of matched elements to the one at the specified index.",
+		choices: [".add()", ".eq()", ".find()", ".delay()"],
+		correctAnswer: 1
+	}, {
+		question: "Get the descendants of each element in the current set of matched elements, filtered by a selector, jQuery object, or element.",
+		choices: [".add()", ".eq()", ".find()", ".delay()"],
+		correctAnswer: 2
+	}, {
+		question: "Set a timer to delay execution of subsequent items in the queue.",
+		choices: [".add()", ".eq()", ".find()", ".delay()"],
+		correctAnswer: 3
+	}, {
+		question: "Execute the next function on the queue for the matched elements.",
+		choices: [".dequeue()", ".eq()", ".find()", ".delay"],
+		correctAnswer: 0
+	}],
+	route: 'start',
+	currentQuestionIndex: 0,
+	
+	quizStart: false,
+	correctCount: 0,
+	incorrectCount: 0,
+	choiceList: $(".choiceList")
+	
 };
 
-// var qnaTemplete = (
-// 	);
-
-var questions = [{
-	question: "Create a new jQuery object with elements added to the set of matched elements.",
-	choices: [".add()", ".eq()", ".find()", ".delay()"],
-	correctAnswer: 0
-}, {
-	question: "Reduce the set of matched elements to the one at the specified index.",
-	choices: [".add()", ".eq()", ".find()", ".delay()"],
-	correctAnswer: 1
-}, {
-	question: "Get the descendants of each element in the current set of matched elements, filtered by a selector, jQuery object, or element.",
-	choices: [".add()", ".eq()", ".find()", ".delay()"],
-	correctAnswer: 2
-}, {
-	question: "Set a timer to delay execution of subsequent items in the queue.",
-	choices: [".add()", ".eq()", ".find()", ".delay()"],
-	correctAnswer: 3
-}, {
-	question: "Execute the next function on the queue for the matched elements.",
-	choices: [".dequeue()", ".eq()", ".find()", ".delay"],
-	correctAnswer: 0
-}];
-
-var currentQuestion = 0;
-var correctAnswers = 0;
-var quizStart = false;
-var quizOver = false;
-var correctCount = 0;
-var incorrectCount = 0;
+var currentQuestion = state.questions[state.currentQuestionIndex].question
+var numberOfChoices = state.questions[state.currentQuestionIndex].choices.length
 
 // State management
-function startQuiz() {
-	currentQuestion = 0;
-	correctCount = 0;
-	incorrectCount = 0;
+// function setRoute(state, route) {
+//   state.route = route;
+// };
+
+function resetGame() {
+	state.correctCount = 0;
+	state.incorrectCount = 0;
+	// state.currentQuestionIndex = 0;
+	// setRoute(state, 'start');
+	console.log(state.currentQuestionIndex)
 	$('.result').text('');
+	$('.currentQuestionCount').text("1");
+};
 
-	if (quizStart === false){
-		$(".score, .quiz, .result, .submitButton, h5").hide();
-		$('.startButton').text("Start");
-		$('.currentQuestion').text("1")
-	} 
-	if (quizStart === true){
-		$('.correctCount').text(correctCount)
-		$('.incorrectCount').text(incorrectCount)
+function checkAnswer() {
+	var value = $("input[type='radio']:checked").val();
 
-		$(".result, .startButton, .explaination").hide();
-		$(".score, .quiz, .result, .submitButton, h5").show();
-		$('.submitButton').text("Submit");
-		$('.currentQuestion').text('1')
+	// console.log(value)
+	if (value === undefined) {
+		$('.result').show().text("Please answer question")
+	} else if (value == state.questions[state.currentQuestionIndex].correctAnswer) {
+		$(".result").show().text("Correct! 😎")
+		state.correctCount++;
+		$('.correctCount').text(state.correctCount)
+	} else if (value != state.questions[state.currentQuestionIndex].correctAnswer){
+		$(".result").show().text("Incorrect 😭")
+	
+		$(".answerIs").show().text("The answer is " + state.questions[state.currentQuestionIndex].choices[state.questions[state.currentQuestionIndex].correctAnswer])
+		state.incorrectCount++;
+		$(".incorrectCount").text(state.incorrectCount)
+	}
+};
 
+function displayCurrentQuestion() {
+	console.log("QI:" + state.currentQuestionIndex)
+
+	$(".question").text(currentQuestion);
+	// console.log(currentQuestion)
+	state.choiceList.empty();
+
+	// listAnswerChoices(numChoices, choice, choiceList);
+	var choicesArray = state.questions[state.currentQuestionIndex].choices;
+	// console.log(state.questions[currentQuestionIndex])
+	for (var i = 0; i < numberOfChoices; i++) {
+		
+		$('<li><input type="radio" value=' + i + ' name="dynradio" required/>' + " " + choicesArray[i] + '</li>').appendTo(state.choiceList);
 	}
 }
 
 function displayNextQuestion() {
-	currentQuestion++;
-	// console.log(currentQuestion, questions.length)
-	if (currentQuestion < questions.length) {
-		$(".currentQuestion").text(currentQuestion + 1);
+	state.currentQuestionIndex++;
+	// console.log("QI:" + state.currentQuestionIndex)
+	if (state.currentQuestionIndex < state.questions.length) {
+		$(".currentQuestionCount").text(state.currentQuestionIndex + 1);
+		currentQuestion = state.questions[state.currentQuestionIndex].question;
 		displayCurrentQuestion();
-
 	} else {
-		quizStart = true;
-
-		$('.quiz, .result, .submitButton').hide()
+		// quizStart = true;
+		$('.quiz, .result, .submitButton, h5').hide()
 		$('.startButton').show().text("Play Again?")			
 	}
 }
 
-// DOM manipulation
+// Render functions
+// function renderApp() {
 
-function displayCurrentQuestion() {
-	
-	var question = questions[currentQuestion].question;
-	var choiceList = $(".choiceList");
-	var numChoices = questions[currentQuestion]. choices.length;
+// };
 
-	$(".question").text(question);
-	choiceList.empty();
+function renderIntroPage() {
+	$(".score, .quiz, .result, .submitButton, h5").hide();
+	$('.startButton').text("Start");
+};
 
-	var choice;
-	for (var i = 0; i < numChoices; i++) {
-		choice = questions[currentQuestion].choices[i];
-		$('<li><input type="radio" value=' + i + ' name="dynradio" required/>' + " " + choice + '</li>').appendTo(choiceList);
-	}
-	console.log(question)
+function renderQuestionPage() {
+	state.currentQuestionIndex = 0;
+	$('.question').text(currentQuestion);
+	console.log("QI:" + state.currentQuestionIndex)
+
+	resetGame();
+	$('.correctCount').text(state.correctCount)
+	$('.incorrectCount').text(state.incorrectCount)
+	$(".result, .startButton, .explaination").hide();
+	$(".score, .quiz, .submitButton, h5").show();
+	$('.submitButton').text("Submit");
+	displayCurrentQuestion();
+	// $(".question").text(currentQuestion);
+	// console.log(currentQuestion)
 }
+
+
+
+
 
 // Event listeners
 $(".startButton").on("click", function(event) {
 	event.preventDefault();
 
-	quizStart = true;
-	startQuiz();
-
-	displayCurrentQuestion();
+	renderQuestionPage();
 })
 
 $(".submitButton").on("click", function(event) { 
 	event.preventDefault();
-	console.log("submitBTN pressed")
 	
 	$('h5').hide();
-	var value = $("input[type='radio']:checked").val();
-
-	console.log(value)
-	if (value === undefined) {
-		$('.result').show().text("Please answer question")
-	} else if (value == questions[currentQuestion].correctAnswer) {
-		$(".result").show().text("Correct! 😎")
-		correctCount++;
-		$('.correctCount').text(correctCount)
-	} else if (value != questions[currentQuestion].correctAnswer){
-		$(".result").show().text("Incorrect 😭")
-	
-		$(".answerIs").show().text("The answer is " + questions[currentQuestion].choices[questions[currentQuestion].correctAnswer])
-		incorrectCount++;
-		$(".incorrectCount").text(incorrectCount)
-	}
+	checkAnswer();
 })
 
 $(".nextQuestionButton").on("click", function(event) {
@@ -131,27 +148,16 @@ $(".nextQuestionButton").on("click", function(event) {
 
 	$('h5').show();
 	var value = $("input[type='radio']:checked").val();
-
 	console.log(value);
-
-
 	if (value === undefined) {
 		$('.result').show().text("Please answer question")
 	} else {		
 		$('.result, .answerIs').hide();
-	// 	if (value == questions[currentQuestion].correctAnswer) {
-	// 			correctCount++;
-	// 		$('.correctCount').text(correctCount)
-	// } else if (value != questions[currentQuestion].correctAnswer){
-	// 			incorrectCount++;
-	// 		$(".incorrectCount").text(incorrectCount)
-	// }
 		displayNextQuestion();
 	}
-	
 })
 
 
 $(function() {
-	startQuiz();
+	renderIntroPage();
 });
